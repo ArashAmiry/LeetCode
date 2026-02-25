e0a318394230c6e04a38592e0e303f7cc2777ff1
Arash Amiry <ArashAmiry@hotmail.com>
2026-02-26 00:02:21 +0100

    ContainerWater Solution

---
+++ ContainerWater/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int maxArea = 0;
+        int leftPointer = 0;
+        int rightPointer = height.Length - 1;
+
+        while (leftPointer < rightPointer)
+        {
+
+                int minHeight =  Math.Min(height[leftPointer], height[rightPointer]);
+                int width = rightPointer - leftPointer;
+                int currentArea = width * minHeight;
+
+                if(currentArea > maxArea)
+                {
+                    maxArea = currentArea;
+                }
+
+            if (height[leftPointer] <= height[rightPointer])
+            {
+                leftPointer++;
+            }
+            else
+            {
+                rightPointer--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(string.Join(", ", solution.MaxArea(new int[] { 2, 3, 4, 3, 2 })));
+    }
+}
