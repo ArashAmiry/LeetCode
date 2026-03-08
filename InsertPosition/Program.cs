caa6c43ffa7e141cf70163f3a6f342e9d3308904
Arash Amiry <ArashAmiry@hotmail.com>
2026-03-08 20:45:17 +0100

    Solution to Insert position

---
+++ InsertPosition/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace InsertPosition
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    return i;
+                }
+                else if (i == nums.Length - 1)
+                {
+                    return nums.Length;
+                }
+                else if (nums[i] > target)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
